9c81f08e1189f9052c817ef324d88cf8106e30d2
Cristian Camilo Betancourt Rincon <cristian.camilo.betancourt.rin@gmail.com>
2023-01-12 18:16:22 -0500

    First Card Skills & Animation Correction

--- src/Components/Presentation/Presentation.jsx
+++ src/Components/Presentation/Presentation.jsx
@@ -1,24 +1,20 @@
 import "./Presentation.css";
-import animation from "./Assets/Animation-panther.gif";
+import animation from "./Assets/Animation850px.gif";
 import avatar from "./Assets/img_avatar.png";
 
 export const Presentation = () => {
   return (
     <>
-      
         <img className="Animation" src={animation} alt="" />
 
         <div className="profile">
-
-          <h1>Hola! mi nombre es Cristian Camilo Betancourt Rincon</h1>
+          <span> Bievenido a mi portafolio </span>
+          
+          <h3>Mi nombre es Cristian Camilo Betancourt Rincon</h3>
           <img src={avatar} alt="" />
           
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti
-            inventore repudiandae molestias harum, aspernatur quasi architecto
-            sequi, error, ea minima mollitia eius tempore consequuntur repellat
-            atque autem incidunt iste ad.
-          
+            Soy desarrollador web Fullstack , tecnólogo en Producción Multimedia y Comunicador Social con enfásis en desarrollo audiovisual. Tengo conocimientos en el campo del diseño gráfico, animación y diseño web tanto Frontend como Backend, bases de datos y algunas librerías de desarrollo. Manejo programas de edición de video, radio e imagen de manera profesional. Te invito a conocer más sobre mí!          
           </p>
         </div>
       
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,8 +1,36 @@
+import "./Skills.css";
+import imageC1 from "./Assets/Design_img.png";
+import iconSkill from "./Assets/html-5.png";
 
-export default function Skills (){
-    return(
-        <>
-            <h1>Skills</h1>
-        </>
-    );
+export default function Skills() {
+  return (
+    <>
+      <div className="card">
+        <img src={imageC1} alt="" />
+        <h3>Diseño & animación</h3>
+        <p>
+          Me gusta transmitir ideas complejas por medio del arte del diseño y la
+          animación.
+        </p>
+
+        <ul>
+          <li>Herramientas de diseño:</li>
+          <li>Figma</li>
+          <li>Illustrator</li>
+          <li>Photoshop</li>
+          <li>Animate</li>
+          <li>After Effects</li>
+          <li>Character Animator</li>
+          <li>Blender</li>
+        </ul>
+
+        <div className="skillsIcons">
+          <img src={iconSkill} alt="" />
+          <img src={iconSkill} alt="" />
+          <img src={iconSkill} alt="" />
+          <img src={iconSkill} alt="" />
+        </div>
+      </div>
+    </>
+  );
 }
